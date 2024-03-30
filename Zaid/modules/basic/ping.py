3be5c952542cd672a0d108a35f21c6eba00185a1
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <165283541+NARUJAT@users.noreply.github.com>
2024-03-30 22:45:41 +0530

    Update ping.py

--- Zaid/modules/basic/ping.py
+++ Zaid/modules/basic/ping.py
@@ -81,7 +81,7 @@ async def pingme(client: Client, message: Message):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await xx.edit(
-        f"❏ **╰☞ ⚡️ 𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 ⚡️**\n"
+        f"❏ **╰☞ ⚡️ 𝗡𝗔𝗥𝗨𝗝𝗔𝗔𝗧 ⚡️**\n"
         f"├• **╰☞ 𝐒ᴘᴇᴇᴅ**`%sms`\n"
         f"├• **╰☞ 𝐔ᴘᴛɪᴍᴇ** `{uptime}` \n"
         f"└• **╰☞ 𝐍ᴀᴍᴇ:** {client.me.mention}" % (duration)
